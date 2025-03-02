324ecfa5c5945a993deeae984ba2045ede7f0d45
arhaaan <85577285+arhaaan@users.noreply.github.com>
2025-03-02 18:05:13 +0700

    Add files via upload

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Karim Arhan on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
